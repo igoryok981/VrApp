fab1067f4980d4b7084cb4eb9a841b3d46069362
Igor Tkachenko <igryok981@gmail.com>
2020-05-27 22:30:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vrapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.vrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button openButton, createButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openButton = (Button)findViewById(R.id.open_button);
+        createButton = (Button) findViewById(R.id.create_button);
+
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openViewActivity();
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreatePanoActivity();
+            }
+        });
+    }
+
+    private void openViewActivity() {
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
+    }
+
+    private void openCreatePanoActivity() {
+        //Intent intent = new Intent(this, CreateActivity.class);
+        //startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/vrapp/ViewActivity.java
@@ -0,0 +1,53 @@
+package com.example.vrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+
+import com.panoramagl.PLImage;
+import com.panoramagl.PLManager;
+import com.panoramagl.PLSphericalPanorama;
+import com.panoramagl.utils.PLUtils;
+
+public class ViewActivity extends AppCompatActivity {
+
+    private PLManager plManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        plManager = new PLManager(this);
+        plManager.setContentView((ViewGroup) findViewById(R.id.content_view));
+        plManager.onCreate();
+
+        PLSphericalPanorama panorama = new PLSphericalPanorama();
+        panorama.getCamera().lookAt(30.0f, 90.0f);
+
+        panorama.setImage(new PLImage(PLUtils.getBitmap(this, R.raw.pano2), false));
+        plManager.setPanorama(panorama);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        plManager.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        plManager.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        plManager.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return plManager.onTouchEvent(event);
+    }
+}
